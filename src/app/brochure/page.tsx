df4c8d1f1e3035ac56650d46ace6edef3ed08f16
Ahaz Fernando <ahazfernando@gmail.com>
2026-01-01 09:43:17 +0530

    Add dealers and brochure App Router pages

---
+++ src/app/brochure/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/layout/Layout";
+import BrochurePage from "@/pages/Brochure";
+
+export default function Brochure() {
+  return (
+    <Layout>
+      <BrochurePage />
+    </Layout>
+  );
+}
---
+++ src/app/dealers/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/layout/Layout";
+import DealersPage from "@/pages/Dealers";
+
+export default function Dealers() {
+  return (
+    <Layout>
+      <DealersPage />
+    </Layout>
+  );
+}
